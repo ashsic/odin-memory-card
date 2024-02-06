b78aea7a3fab583d579c1175c5cb85c07e173a7b
ashsic <99445200+ashsic@users.noreply.github.com>
2024-02-06 11:09:52 -0500

    basic api call completed

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Imagefetch from './components/Imagefetch'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Imagefetch name="Thresh"/>
+
+
+
+    </>
+  )
+}
+
+export default App
